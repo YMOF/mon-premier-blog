34cf44a2039fb492f9b12a2f5f598109c3346a2b
Megane <yasminath.ossn@eduge.ch>
2025-11-18 16:58:01 +0100

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import render
+from . import views
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('blog.urls')),
+]
