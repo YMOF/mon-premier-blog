2bcc27aff2420f7a53c035f84dd6ea151f27a210
Megane <yasminath.ossn@eduge.ch>
2025-12-17 15:53:47 +0100

    modification page views modifier

--- cakes/views.py
+++ cakes/views.py
@@ -22,22 +22,33 @@ def creation_recette(request):
 
     return render(request, 'creation_recette.html')
 
-
 def modifier_recette(request, recette_id):
     recette = get_object_or_404(Recette, id=recette_id)
 
     if request.method == "POST":
-        recette.nom = request.POST.get('nom')
-        recette.ingredients = request.POST.get('ingredients')
-        recette.instructions = request.POST.get('instructions')
-        recette.save()
+        nom = request.POST.get('nom')
+        ingredients = request.POST.get('ingredients')
+        instructions = request.POST.get('instructions')
 
         if not nom or not ingredients or not instructions:
-            erreur ="Veuillez remplir tous les champs."
-            return render(request, 'modifier_recette.html', {'error': "Veuillez remplir tous les champs."})
-        return redirect('accueil')  # Redirige vers l'index après modification
+            return render(
+                request,
+                'modifier_recette.html',
+                {
+                    'recette': recette,
+                    'error': "Veuillez remplir tous les champs."
+                }
+            )
+
+        recette.nom = nom
+        recette.ingredients = ingredients
+        recette.instructions = instructions
+        recette.save()
+
+        return redirect('accueil')
 
     return render(request, 'modifier_recette.html', {'recette': recette})
+
 
 
 def supprimer_recette(request, recette_id):
