20807491aaacbfcddf7cea39763a8be6a12964fe
Megane <yasminath.ossn@eduge.ch>
2025-12-17 14:05:45 +0100

    ajout des modifications tentative1.2

--- cakes/views.py
+++ cakes/views.py
@@ -6,12 +6,16 @@ def accueil(request):
     cakes_recette = Recette.objects.all()
     return render(request, 'index.html', {'cakes_recette': cakes_recette})
 
-# Création d'une recette
+# Création d'une recette        formulaire uniquement= raise forms.ValidationError("Veuillez remplir tous les champs")
 def creation_recette(request):
-    if request.method == 'POST':
+    if request.method == 'POST': # POST -> Renvoie ce quel 'utilisatuer envoire. Si le formulaire est vide -> GET
         nom = request.POST.get('nom')
         ingredients = request.POST.get('ingredients')
         instructions = request.POST.get('instructions')
+# une fois ajt va dans la bdd Models.py
+        if not nom or not ingredients or not instructions:
+            erreur ="Veuillez remplir tous les champs."
+            return render(request, 'creation_recette.html', {'error': "Veuillez remplir tous les champs."})
 
         Recette.objects.create(nom=nom, ingredients=ingredients, instructions=instructions)
         return redirect('accueil')  # Redirige vers la page index après ajout
@@ -27,6 +31,10 @@ def modifier_recette(request, recette_id):
         recette.ingredients = request.POST.get('ingredients')
         recette.instructions = request.POST.get('instructions')
         recette.save()
+
+        if not nom or not ingredients or not instructions:
+            erreur ="Veuillez remplir tous les champs."
+            return render(request, 'modifier_recette.html', {'error': "Veuillez remplir tous les champs."})
         return redirect('accueil')  # Redirige vers l'index après modification
 
     return render(request, 'modifier_recette.html', {'recette': recette})
